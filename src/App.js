5941fc7863dba3933a6ce1ffd55e4fc140c45b56
Jpark <web.jpark@0gmail.com>
2023-11-09 16:54:52 -0700

    create Main component & revise Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './layouts/Header';
+import Main from './layouts/Main';
 
 export default function App() {
 	return (
 		<div className="app">
 			<Header />
+
+			<Main>
+				<p>question??</p>
+			</Main>
 		</div>
 	);
 }
