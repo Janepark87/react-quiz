7d570ed904e4f4a653c064b0f5175c361d6e77f5
Jpark <web.jpark@gmail.com>
2025-01-26 13:25:26 -0700

    organized the component files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import { useEffect, useReducer } from 'react';
+import { useQuiz } from './contexts/QuizContext';
 import Header from './layouts/Header';
 import Main from './layouts/Main';
-import Loader from './components/Loader';
-import Error from './components/Error';
-import StartScreen from './components/quiz/StartScreen';
-import Question from './components/quiz/Question';
-import NextButton from './components/quiz/NextButton';
-import ProgressBar from './components/quiz/ProgressBar';
-import FinishedScreen from './components/quiz/FinishedScreen';
-import Timer from './components/quiz/Timer';
-import Exit from './components/quiz/Exit';
-import PreviousButton from './components/quiz/PreviousButton';
-import ButtonGroup from './components/ButtonGroup';
-import { useQuiz } from './contexts/QuizContext';
+import { Loader, Error, ButtonGroup, StartScreen, Question, NextButton, ProgressBar, FinishedScreen, Timer, Exit, PreviousButton } from './components';
 
 export default function App() {
 	const { status } = useQuiz();
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Loader from './Loader';
+import Error from './Error';
+import ButtonGroup from './ButtonGroup';
+
+// quiz
+import Exit from './quiz/Exit';
+import FinishedScreen from './quiz/FinishedScreen';
+import NextButton from './quiz/NextButton';
+import ProgressBar from './quiz/ProgressBar';
+import PreviousButton from './quiz/PreviousButton';
+import Question from './quiz/Question';
+import StartScreen from './quiz/StartScreen';
+import Timer from './quiz/Timer';
+
+export { Loader, ButtonGroup, Error, Exit, FinishedScreen, NextButton, ProgressBar, PreviousButton, Question, StartScreen, Timer };
